65ef501d515b419f8ded931f9fa4605c7cdfb39d
alixb1908 <alixb1908@gmail.com>
2023-04-30 18:41:04 +0200

    upgrading shields

--- funnel/algo_strategy.py
+++ funnel/algo_strategy.py
@@ -51,7 +51,7 @@ class AlgoStrategy(gamelib.AlgoCore):
         self.removed_turrets_r = [[26, 13], [26, 12],
                                   [25, 12]]
         self.removed_turrets = self.removed_turrets_l
-        self.side = True
+        self.side = True #attack on the left
         self.attack_coords_l = [[14, 0], [24, 10]]
         self.attack_coords_r = [[13, 0], [3, 10]]
         self.attack_coords = self.attack_coords_l
@@ -71,7 +71,7 @@ class AlgoStrategy(gamelib.AlgoCore):
         # Comment or remove this line to enable warnings.
         game_state.suppress_warnings(True)
 
-        if self.side:
+        if self.side: #attack on the left
             self.removed_turrets = self.removed_turrets_l
             self.attack_coords = self.attack_coords_l
         else:
@@ -134,10 +134,69 @@ class AlgoStrategy(gamelib.AlgoCore):
 
     def put_shields(self, game_state):
         self.shields = [[12, 3], [13, 3], [14, 3], [15, 3]]
-        game_state.attempt_spawn(SUPPORT, self.shields)
+        for shield in self.shields:
+            game_state.attempt_spawn(SUPPORT, [shield])
+            game_state.attempt_upgrade([shield])
         self.extra_shields = [[13, 2], [14, 2]]
         if game_state.get_resource(SP) >= 15:
             game_state.attempt_spawn(SUPPORT, self.extra_shields)
+            game_state.attempt_upgrade(self.extra_shields)
+
+
+
+
+
+
+        # def detect_enemy_unit(self, game_state, unit_type=None, valid_x = None, valid_y = None):
+        #     total_units = 0
+        #     for location in game_state.game_map:
+        #         if game_state.contains_stationary_unit(location):
+        #             for unit in game_state.game_map[location]:
+        #                 if unit.player_index == 1 and (unit_type is None or unit.unit_type == unit_type) and (valid_x is None or location[0] in valid_x) and (valid_y is None or location[1] in valid_y):
+        #                     total_units += 1
+        #     return total_units
+
+
+        # def on_action_frame(self, turn_string):
+        #     """
+        #     This is the action frame of the game. This function could be called
+        #     hundreds of times per turn and could slow the algo down so avoid putting slow code here.
+        #     Processing the action frames is complicated so we only suggest it if you have time and experience.
+        #     Full doc on format of a game frame at in json-docs.html in the root of the Starterkit.
+        #     """
+        #     # Let's record at what position we get scored on
+        #     state = json.loads(turn_string)
+        #     events = state["events"]
+        #     breaches = events["breach"]
+        #     for breach in breaches:
+        #         location = breach[0]
+        #         unit_owner_self = True if breach[4] == 1 else False
+        #         # When parsing the frame data directly,
+        #         # 1 is integer for yourself, 2 is opponent (StarterKit code uses 0, 1 as player_index instead)
+        #         if not unit_owner_self:
+        #             gamelib.debug_write("Got scored on at: {}".format(location))
+        #             self.scored_on_locations.append(location)
+        #             gamelib.debug_write("All locations: {}".format(self.scored_on_locations))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         # """
         # NOTE: All the methods after this point are part of the sample starter-algo
@@ -273,14 +332,7 @@ class AlgoStrategy(gamelib.AlgoCore):
         #     # Now just return the location that takes the least damage
         #     return location_options[damages.index(min(damages))]
 
-        # def detect_enemy_unit(self, game_state, unit_type=None, valid_x = None, valid_y = None):
-        #     total_units = 0
-        #     for location in game_state.game_map:
-        #         if game_state.contains_stationary_unit(location):
-        #             for unit in game_state.game_map[location]:
-        #                 if unit.player_index == 1 and (unit_type is None or unit.unit_type == unit_type) and (valid_x is None or location[0] in valid_x) and (valid_y is None or location[1] in valid_y):
-        #                     total_units += 1
-        #     return total_units
+        
 
         # def filter_blocked_locations(self, locations, game_state):
         #     filtered = []
@@ -290,26 +342,7 @@ class AlgoStrategy(gamelib.AlgoCore):
         #     return filtered
 
 
-        # def on_action_frame(self, turn_string):
-        #     """
-        #     This is the action frame of the game. This function could be called
-        #     hundreds of times per turn and could slow the algo down so avoid putting slow code here.
-        #     Processing the action frames is complicated so we only suggest it if you have time and experience.
-        #     Full doc on format of a game frame at in json-docs.html in the root of the Starterkit.
-        #     """
-        #     # Let's record at what position we get scored on
-        #     state = json.loads(turn_string)
-        #     events = state["events"]
-        #     breaches = events["breach"]
-        #     for breach in breaches:
-        #         location = breach[0]
-        #         unit_owner_self = True if breach[4] == 1 else False
-        #         # When parsing the frame data directly,
-        #         # 1 is integer for yourself, 2 is opponent (StarterKit code uses 0, 1 as player_index instead)
-        #         if not unit_owner_self:
-        #             gamelib.debug_write("Got scored on at: {}".format(location))
-        #             self.scored_on_locations.append(location)
-        #             gamelib.debug_write("All locations: {}".format(self.scored_on_locations))
+
 if __name__ == "__main__":
     algo = AlgoStrategy()
     algo.start()
